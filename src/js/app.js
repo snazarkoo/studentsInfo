7d85a7cd46b5732f8e56515c66b3c5cb6a1b8488
Oleh Chemerskyi <Oleh_Chemerskyi@EPUALVIW1107.kyiv.epam.com>
2015-11-04 16:30:47 +0200

    merge

--- src/js/app.js
+++ src/js/app.js
@@ -2,40 +2,21 @@ angular
     .module('studentsInfo', [])
     .controller('appController', appController);
 
-function appController() {
+function appController($scope, $http) {
     vm = this;
     vm.newValue = "name";
-    vm.students = [{
-        name: "Test1",
-        email: "test@test.com",
-        testMark: 10
-    }, {
-        name: "Test2",
-        email: "test@test.com",
-        testMark: 10
-    }, {
-        name: "Test3",
-        email: "test@test.com",
-        testMark: 10
-    }, {
-        name: "Test",
-        email: "test@test.com",
-        testMark: 10
-    }, {
-        name: "Test",
-        email: "test@test.com",
-        testMark: 10
-    }, {
-        name: "Test",
-        email: "test@test.com",
-        testMark: 10
-    }, {
-        name: "Test",
-        email: "test@test.com",
-        testMark: 10
-    }, {
-        name: "Test",
-        email: "test@test.com",
-        testMark: 10
-    }]
+    vm.students = [{name: "sdasaddas"}];
+    var url = 'https://spreadsheets.google.com/feeds/list/1FzkGijfnvihEVnI43EF0mJK4H2nxGMDj7R7ZJY0Bb5k/od6/public/values?alt=json-in-script&callback=?';
+    jQuery.getJSON(url).success(function(data) {
+
+        data.feed.entry.forEach(function(value, index) {
+            vm.students.push({id: value.gsx$id.$t, name: value.gsx$name.$t, mark: value.gsx$mark.$t, surname: value.gsx$surname.$t});
+        });
+        $scope.$digest();
+        console.log(vm.students);
+    }).error(function(message) {
+        console.error('error' + message);
+    }).complete(function() {
+        console.log('completed!');
+    });
 }
