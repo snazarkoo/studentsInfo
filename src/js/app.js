f2f04248e509c444eaa105000176fd29bef2ba30
Nazar Shcherbiak <Nazar_Shcherbiak@epam.com>
2015-11-04 16:24:04 +0200

    Added new features

--- src/js/app.js
+++ src/js/app.js
@@ -1,41 +1,67 @@
 angular
-    .module('studentsInfo', [])
-    .controller('appController', appController);
+    .module('studentsInfo', ['ngTable']);
 
-function appController() {
-    vm = this;
-    vm.newValue = "name";
-    vm.students = [{
-        name: "Test1",
-        email: "test@test.com",
-        testMark: 10
-    }, {
-        name: "Test2",
-        email: "test@test.com",
-        testMark: 10
-    }, {
-        name: "Test3",
-        email: "test@test.com",
-        testMark: 10
-    }, {
-        name: "Test",
-        email: "test@test.com",
-        testMark: 10
-    }, {
-        name: "Test",
-        email: "test@test.com",
-        testMark: 10
-    }, {
-        name: "Test",
-        email: "test@test.com",
-        testMark: 10
-    }, {
-        name: "Test",
-        email: "test@test.com",
-        testMark: 10
-    }, {
-        name: "Test",
-        email: "test@test.com",
-        testMark: 10
-    }]
-}
+(function() {
+    angular
+        .module('studentsInfo')
+        .controller('appController', appController);
+
+    function appController(NgTableParams) {
+        var vm = this;
+        var data = [{
+                firstName: "Test1",
+                secondName: "Test2",
+                mobileNumber: 5555,
+                email: "test@test.com",
+                testMark: 10,
+                englishLevel: "B1",
+                selfDescription: "Awesome man",
+                teacherOpinion: "good",
+                decision: "YES"
+            }, {
+                firstName: "Test2",
+                secondName: "Test2",
+                mobileNumber: 6666,
+                email: "test@test.com",
+                testMark: 10
+            }, {
+                firstName: "Test3",
+                secondName: "Test2",
+                mobileNumber: 5555,
+                email: "test@test.com",
+                testMark: 10
+            }, {
+                firstName: "Test",
+                secondName: "Test2",
+                mobileNumber: 5555,
+                email: "test@test.com",
+                testMark: 10
+            }, {
+                firstName: "Test",
+                secondName: "Test2",
+                mobileNumber: 5555,
+                email: "test@test.com",
+                testMark: 10
+            }]
+
+        vm.studentProfile = {};
+        vm.openUserProfile = openUserProfile;
+        vm.tableParams = new NgTableParams({}, {
+            dataset: data
+        });
+
+        function openUserProfile(studentInfo) {
+            vm.studentProfile = {};
+            for (var key in studentInfo) {
+                if (studentInfo[key]) {
+                    vm.studentProfile[key] = studentInfo[key];
+                }
+            }
+        }
+    }
+})();
+
+(function() {
+    "use strict";
+    angular.module("studentsInfo")
+})();
