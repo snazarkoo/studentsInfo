f111e6d551f23e5a1232349ac8a4c847f7a1bbcb
Oleh Chemerskyi <Oleh_Chemerskyi@EPUALVIW1107.kyiv.epam.com>
2015-11-09 10:08:03 +0200

    Final version

--- src/js/appController.js
+++ src/js/appController.js
@@ -4,27 +4,26 @@ angular
 
 function appController($scope, $http) {
     var vm = this;
-    vm.predicate = 'age';
-    vm.reverse = true;
-    vm.order = function(predicate) {
-        vm.reverse = (vm.predicate === predicate) ? !vm.reverse : false;
-        vm.predicate = predicate;
-      };
-    
+
     vm.students = [];
     vm.studentProfile = {};
+    vm.predicate = 'age';
+    vm.reverse = true;
     vm.decisionByColor = {
         "Yes": "success",
         "No": "danger",
         "Maybe": "warning"
     };
-    vm.getSocialMediaImage = getSocialMediaImage;
+    vm.order = order;
     vm.openUserProfile = openUserProfile;
     vm.sendInfo = sendInfo;
 
+    var urlPost = 'https://script.google.com/macros/s/AKfycby-Fnpz1VJRZoaPrmV9DB84D2cUnyZp392-5FSka-S7cRUQkMI/exec';
+    var urlGet = 'https://spreadsheets.google.com/feeds/list/1FzkGijfnvihEVnI43EF0mJK4H2nxGMDj7R7ZJY0Bb5k/od6/public/values?alt=json-in-script&callback=?';
     getData();
 
     function openUserProfile(studentInfo) {
+        console.log(studentInfo);
         vm.studentProfile = {};
         for (var key in studentInfo) {
             if (studentInfo[key]) {
@@ -34,19 +33,18 @@ function appController($scope, $http) {
     }
 
     function sendInfo(data) {
-        console.log("Send info");
-        var feedback = "Feedback=" + data.feedback + "&id=" + data.id + "&Conclusion=" + data.conclusion;
-        console.log(feedback);
+        var feedback = "Feedback=" + data.feedback + "&Conclusion=" + data.conclusion + "&E-mail=" + data.email;
         feedback = encodeURI(feedback);
         vm.students.forEach(function(value, index) {
-            if(value.id === data.id) {
+            console.log(value.id, data.id);
+            if(value.email === data.email) {
                 value.feedback = data.feedback;
                 value.conclusion = data.conclusion;
             }
         });
         var req = {
             method: 'POST',
-            url: 'https://script.google.com/macros/s/AKfycby-Fnpz1VJRZoaPrmV9DB84D2cUnyZp392-5FSka-S7cRUQkMI/exec',
+            url: urlPost,
             headers: {
                 'Content-Type': "application/x-www-form-urlencoded"
             },
@@ -55,24 +53,22 @@ function appController($scope, $http) {
         };
         $http(req).then(function(data) {
             console.log("success" + data);
+           // getData();
         }, function(error) {
             console.log(error);
+          //  getData();
         });
     }
 
-    function getSocialMediaImage(url) {
-        if(url.indexOf("fb") > -1 || url.indexOf("facebook") > -1) {
-            return "img/facebook.png";
-        }
-        if(url.indexOf("vk.com") > -1) {
-            return "img/vk.png";
-        }
-    }
+    function order(predicate) {
+        vm.reverse = (vm.predicate === predicate) ? !vm.reverse : false;
+        vm.predicate = predicate;
+      }
 
     function getData() {
-        var url = 'https://spreadsheets.google.com/feeds/list/1FzkGijfnvihEVnI43EF0mJK4H2nxGMDj7R7ZJY0Bb5k/od6/public/values?alt=json-in-script&callback=?';
-        jQuery.getJSON(url).success(function(data) {
+        jQuery.getJSON(urlGet).success(function(data) {
             console.log(data);
+            vm.students = [];
             data.feed.entry.forEach(function(value, index) {
                 vm.students.push({
                     conclusion: value.gsx$conclusion.$t,
@@ -80,11 +76,12 @@ function appController($scope, $http) {
                     feedback: value.gsx$feedback.$t,
                     nameEng: value.gsx$firstandlastnameeng.$t,
                     nameUkr: value.gsx$firstandlastnameukr.$t,
-                    id: value.gsx$id.$t,
+                    email: value["gsx$e-mail"].$t,
                     socialLink: value.gsx$linktosocialnetworkaccountfacebookvketc.$t,
                     phone: value.gsx$phone.$t,
                     timestamp: value.gsx$timestamp.$t,
-                    entryDecision: value.gsx$whyhaveyoudecidedtojointhiscourse.$t
+                    entryDecision: value.gsx$whyhaveyoudecidedtojointhiscourse.$t,
+                    mark: value.gsx$mark.$t
                 });
             });
             $scope.$digest();
